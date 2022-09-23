c84df67926a230a64d8a6162b99c5c63fe7443c6
ykw0104 <ykw0104@163.com>
2022-09-23 20:39:50 +0800

    43

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./2_router";
 import store from "./3_store";
 
+// 重置样式
+import "normalize.css";
+import "@/assets/styles/common.scss";
+
 const app = createApp(App);
 
 app.use(store);
