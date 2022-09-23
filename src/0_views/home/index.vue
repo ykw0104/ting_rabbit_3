6e73b14ed74970cf70624269bd58e9fb5f552e75
ykw0104 <ykw0104@163.com>
2022-09-23 19:59:42 +0800

    41

--- src/0_views/home/index.vue
+++ src/0_views/home/index.vue
@@ -1,17 +1,11 @@
 <template>
-  <div class="container">
-    <div class="test"></div>
-  </div>
+  <div class="container">Home Page</div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
-.test {
-  width: 100px;
-  height: 100px;
-  background-color: #000;
-
-  @include hovershadow();
+.container {
+  @include hoverShadow;
 }
 </style>
