4621b1436b645a9bded2f6db9e1a11e26e083d4c
ykw0104 <ykw0104@163.com>
2022-09-23 17:44:07 +0800

    40

--- src/0_views/home/index.vue
+++ src/0_views/home/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="container">home</div>
+  <div class="container">
+    <div class="test"></div>
+  </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "../../assets/styles/mixins.scss";
+
+.test {
+  width: 100px;
+  height: 100px;
+  background-color: #000;
+
+  @include hovershadow();
+}
+</style>
