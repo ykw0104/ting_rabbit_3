6bc5348213dd54ecfea9c7af93a78b1a9dcdbbb2
ykw0104 <ykw0104@163.com>
2022-09-28 10:55:53 +0800

    47

--- src/0_views/Layout.vue
+++ src/0_views/Layout.vue
@@ -6,18 +6,23 @@
   <AppHeader></AppHeader>
 
   <!-- 内容 -->
-  <div class="main">
+  <div class="app-body">
     <!-- 二级路由 -->
     <RouterView />
   </div>
 
   <!-- 底部 -->
-  <div>底部</div>
+  <AppFooter></AppFooter>
 </template>
 
 <script setup>
 import AppNavbar from "@/1_components/app-navbar.vue";
 import AppHeader from "@/1_components/app-header.vue";
+import AppFooter from "@/1_components/app-footer.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.app-body {
+  min-height: 600px;
+}
+</style>
