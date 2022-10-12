047c7155124968b9f92ab0374043005c44c1fbc9
ykw0104 <ykw0104@163.com>
2022-10-12 15:43:39 +0800

    54

--- src/0_views/home/components/home-category.vue
+++ src/0_views/home/components/home-category.vue
@@ -2,7 +2,11 @@
 <template>
   <div class="home-category">
     <ul class="menu">
-      <li v-for="item in menuList" :key="item.id">
+      <li
+        v-for="item in menuList"
+        :key="item.id"
+        @mouseenter="getCategoryId(item.id)"
+      >
         <!-- 一级类目 -->
         <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
 
@@ -17,11 +21,28 @@
         </template>
       </li>
     </ul>
+
+    <!-- 二级类目的弹层 -->
+    <div class="layer">
+      <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <ul v-if="currCategory && currCategory.goods">
+        <li v-for="item in currCategory.goods" :key="item.id">
+          <RouterLink to="/">
+            <img :src="item.picture" alt="" />
+            <div class="info">
+              <p class="name ellipsis-2">{{ item.name }}</p>
+              <p class="desc ellipsis">{{ item.desc }}</p>
+              <p class="price"><i>¥</i>{{ item.price }}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { computed, reactive } from "vue";
+import { computed, reactive, ref } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
@@ -47,6 +68,16 @@ const menuList = computed(() => {
   list.push(brand);
 
   return list;
+});
+
+// 获取每个类目的弹出层的商品数据
+const categoryId = ref(null);
+const getCategoryId = (id) => {
+  categoryId.value = id;
+};
+
+const currCategory = computed(() => {
+  return menuList.value.find((item) => item.id === categoryId.value);
 });
 </script>
 
@@ -76,6 +107,94 @@ const menuList = computed(() => {
           font-size: 16px;
         }
       }
+    }
+  }
+
+  .layer {
+    display: none;
+    position: absolute;
+    left: 250px;
+    top: 0;
+    width: 990px;
+    height: 500px;
+    padding: 0 15px;
+    background: rgba(255, 255, 255, 0.8);
+
+    h4 {
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+
+      small {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+
+      li {
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px soid #eee;
+        border-radius: 4px;
+        background: #fff;
+
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+
+        a {
+          display: flex;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+          padding: 10px;
+
+          &:hover {
+            background: #e3f9f4;
+          }
+
+          img {
+            width: 95px;
+            height: 95px;
+          }
+
+          .info {
+            width: 190px;
+            line-height: 24px;
+            padding-left: 10px;
+
+            .name {
+              font-size: 16px;
+              color: #666;
+            }
+
+            .desc {
+              color: #999;
+            }
+
+            .price {
+              font-size: 22px;
+              color: $priceColor;
+
+              i {
+                font-size: 16px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  &:hover {
+    .layer {
+      display: block;
     }
   }
 }
