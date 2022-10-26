81c912634caa082eeb9ff202efd4e9aec0e785b5
ykw0104 <ykw0104@163.com>
2022-10-26 16:13:19 +0800

    76

--- src/0_views/category/index.vue
+++ src/0_views/category/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>index</div>
+  <div class="top-category">
+    <div class="container">
+      <!-- 面包屑 -->
+      <XtxBread>
+        <XtxBreadItem to="/">首页</XtxBreadItem>
+        <XtxBreadItem to="/category/1005000">电器</XtxBreadItem>
+        <XtxBreadItem>空调</XtxBreadItem>
+      </XtxBread>
+
+      <!-- 轮播图 -->
+      <!-- 全部分类 -->
+      <!-- 各个分类推荐商品 -->
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  name: "TopCategory",
+};
+</script>
 <script setup></script>
 
 <style lang="scss" scoped></style>
--- src/1_components/library/index.js
+++ src/1_components/library/index.js
@@ -6,6 +6,8 @@
 import XtxSkeleton from "./xtx-skeleton.vue";
 import XtxCarousel from "./xtx-carousel.vue";
 import XtxMore from "./xtx-more.vue";
+import XtxBread from "./xtx-bread.vue";
+import XtxBreadItem from "./xtx-bread-item.vue";
 
 export default {
   install(app) {
@@ -14,6 +16,8 @@ export default {
     app.component(XtxSkeleton.name, XtxSkeleton);
     app.component(XtxCarousel.name, XtxCarousel);
     app.component(XtxMore.name, XtxMore);
+    app.component(XtxBread.name, XtxBread);
+    app.component(XtxBreadItem.name, XtxBreadItem);
 
     // 定义指令
     defineDirective(app);
