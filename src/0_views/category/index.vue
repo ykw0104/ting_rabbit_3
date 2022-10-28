0f712d877f00d57f4e56ac6f3b1e4f592c631eb4
ykw0104 <ykw0104@163.com>
2022-10-28 14:26:25 +0800

    81

--- src/0_views/category/index.vue
+++ src/0_views/category/index.vue
@@ -24,6 +24,20 @@
       </div>
 
       <!-- 不同分类商品 -->
+      <div class="ref-goods" v-for="sub in subList" :key="sub.id">
+        <div class="head">
+          <h3>- {{ sub.name }} -</h3>
+          <p class="tag">温暖柔软，品质之选</p>
+          <XtxMore :path="`/category/sub/${sub.id}`" />
+        </div>
+        <div class="body">
+          <GoodsItem
+            v-for="goods in sub.goods"
+            :key="goods.id"
+            :goods="goods"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,8 +50,10 @@ export default {
 <script setup>
 import { useStore } from "vuex";
 import { useRoute } from "vue-router";
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import { findBanner } from "@/api/home";
+import { findTopCategory } from "@/api/category";
+import GoodsItem from "./components/goods-item.vue";
 
 // 轮播图
 const sliders = ref([]);
@@ -56,6 +72,23 @@ const topCategory = computed(() => {
   if (item) cate = item;
   return cate;
 });
+
+// 获取各个类目下的推荐商品
+const subList = ref([]);
+const getSubList = () => {
+  findTopCategory(route.params.id).then((data) => {
+    subList.value = data.result.children;
+  });
+};
+watch(
+  () => route.params.id,
+  (newVal) => {
+    newVal && getSubList();
+  },
+  {
+    immediate: true,
+  }
+);
 </script>
 
 <style lang="scss" scoped>
@@ -101,6 +134,32 @@ const topCategory = computed(() => {
         }
       }
     }
+  }
+}
+
+.ref-goods {
+  background-color: #fff;
+  margin-top: 20px;
+  position: relative;
+  .head {
+    .xtx-more {
+      position: absolute;
+      top: 20px;
+      right: 20px;
+    }
+    .tag {
+      text-align: center;
+      color: #999;
+      font-size: 20px;
+      position: relative;
+      top: -20px;
+    }
+  }
+  .body {
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    padding: 0 65px 30px;
   }
 }
 </style>
