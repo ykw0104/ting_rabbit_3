27f987ba94eff21c546c7a068d9d68101e86da5b
ykw0104 <ykw0104@163.com>
2022-11-14 15:55:24 +0800

    90

--- src/0_views/category/sub.vue
+++ src/0_views/category/sub.vue
@@ -13,6 +13,18 @@
         <SubSort />
 
         <!-- 列表 -->
+        <ul>
+          <li v-for="goods in goodsList" :key="goods.id">
+            <GoodsItem :goods="goods" />
+          </li>
+        </ul>
+
+        <!-- 加载 -->
+        <XtxInfiniteLoading
+          :loading="loading"
+          :finished="finished"
+          @infinite="getData"
+        />
       </div>
     </div>
   </div>
@@ -24,12 +36,57 @@ export default {
 };
 </script>
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
 import SubBread from "./components/sub-bread.vue";
 import SubFilter from "./components/sub-filter";
 import SubSort from "./components/sub-sort";
+import GoodsItem from "./components/goods-item";
+import { findSubCategoryGoods } from "@/api/category";
 
-const isAllChecked = ref(true);
+const route = useRoute();
+
+// 加载中
+const loading = ref(false);
+const finished = ref(false);
+const goodsList = ref([]);
+// 查询参数
+let reqParams = {
+  page: 1,
+  pageSize: 20,
+};
+
+const getData = () => {
+  loading.value = true;
+
+  // 二级分类的ID
+  reqParams.categoryId = route.params.id;
+  findSubCategoryGoods(reqParams).then(({ result }) => {
+    if (result.items.length) {
+      goodsList.value.push(...result.items);
+      reqParams.page++;
+    } else {
+      // 没有数据,加载完成
+      finished.value = true;
+    }
+
+    loading.value = false;
+  });
+};
+
+watch(
+  () => route.params.id,
+  (newVal) => {
+    if (newVal && `/category/sub/${newVal}` === route.path) {
+      finished.value = false;
+      goodsList.value = [];
+      reqParams = {
+        page: 1,
+        pageSize: 20,
+      };
+    }
+  }
+);
 </script>
 
 <style lang="scss" scoped>
@@ -37,5 +94,20 @@ const isAllChecked = ref(true);
   padding: 0 25px;
   margin-top: 25px;
   background: #fff;
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5px;
+
+    li {
+      margin-right: 20px;
+      margin-bottom: 20px;
+
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
 }
 </style>
